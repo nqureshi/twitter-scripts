792e952b2478ea8807fa3d12c82a6552d5eecf1a
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2023-12-13 19:08:08 -0700

    quick fix

--- count-mutuals.py
+++ count-mutuals.py
@@ -3,7 +3,15 @@ import json
 with open('following.txt', 'r') as file:
     json_data = json.load(file)
 
+total_following = len(json_data)
+
 def mutuals_only(data):
     return [item for item in data if item.get('mutuals') == True]
 
-print(mutuals_only(json_data))
+mutuals = mutuals_only(json_data)
+
+print("Total Number of People You're Following:", total_following)
+print("Mutuals:", len(mutuals))
+print("First 10 Mutuals:")
+for mutual in true_mutuals[:10]:
+    print(json.dumps(mutual, indent=2))
