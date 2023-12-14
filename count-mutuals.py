cd81f792c1850745814694341aad4d3aa897ef2d
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2023-12-13 19:05:26 -0700

    script

--- count-mutuals.py
+++ count-mutuals.py
@@ -1,15 +1,9 @@
 import json
 
-# Read the contents of 'following.txt' file
 with open('following.txt', 'r') as file:
     json_data = json.load(file)
 
-# Count the number of elements in the JSON variable
-num_elements = len(json_data)
+def mutuals_only(data):
+    return [item for item in data if item.get('mutuals') == True]
 
-json_formatted_str = json.dumps(json_data, indent=2)
-
-print(json_formatted_str)
-
-# Print the count
-print("Number of elements:", num_elements)
+print(mutuals_only(json_data))
